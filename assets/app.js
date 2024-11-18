2392c24f7517a03a90c517ccdac0586558f5981d
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2024-11-18 12:31:08 +0100

    Removed old config. Not needed any more

--- assets/app.js
+++ assets/app.js
@@ -15,24 +15,10 @@ import 'bootstrap';
 
 import $ from 'jquery';
 
-// import './js/bootstrap-editable.js';
-// No funciona igual con bootstrap de momento lo metemos directamente en la página
-// import './js/3/wysiwig.js';
-//import './js/3/bootstrap-wysihtml5-0.0.3.min.js';
-//import './js/3/bootstrap3-wysihtml5.js';
-// import './js/calendar/bootstrap-datepicker.min.js';
-// import './js/calendar/bootstrap-datepicker.es.min.js';
-// import './js/calendar/bootstrap-datepicker.eu.min.js';
-// import './js/bootbox.min.js';
-
 // Declare $ globally
-// global.$ = $;
-// global.jQuery = $;
+global.$ = $;
+global.jQuery = $;
 global.base = '/zergaordenantzak'
-
-// const routes = require('../public/bundles/fosjsrouting/js/fos_js_routes.json');
-// import Routing from '../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
-
 
 var body = $('body');
 body.scrollspy({
@@ -43,50 +29,3 @@ body.scrollspy({
 $(window).on("hashchange", function () {
     window.scrollTo(window.scrollX, window.scrollY - 100);
 });
-
-// $(document).ready(function () {
-//     $('.js-datepicker').datepicker({
-//         format: 'yyyy-mm-dd',
-//         todayBtn: "linked",
-//         language: "eu",
-//         autoclose: true,
-//         todayHighlight: true
-//     });
-// });
-
-/* <script src="{{ asset('bundles/app/js/bootstrap-editable.js') }}"></script>
-
-<script src="{{ asset('bundles/app/js/3/wysihtml5-0.3.0.min.js') }}"></script>
-<script src="{{ asset('bundles/app/js/3/bootstrap-wysihtml5-0.0.3.min.js') }}"></script>
-<script src="{{ asset('bundles/app/js/3/wysihtml5-0.0.3.js') }}"></script>
-<script src="{{ asset('bundles/app/js/calendar/bootstrap-datepicker.min.js') }}"></script>
-<script src="{{ asset('bundles/app/js/calendar/bootstrap-datepicker.es.min.js') }}"></script>
-<script src="{{ asset('bundles/app/js/calendar/bootstrap-datepicker.eu.min.js') }}"></script>
-<script src="{{ asset('bundles/app/js/bootbox.min.js') }}"></script>
-
-
-<script src="{{ asset('bundles/fosjsrouting/js/router.js') }}"></script>
-<script src="{{ path('fos_js_routing_js', { callback: 'fos.Router.setData' }) }}"></script>
-
-<script>
-    var body = $('body');
-    body.scrollspy({
-        'target': '#nav',
-        'offset': 100 //this must match the window.scrollY below or you'll have a bad time mmkay
-    });
-
-    $(window).on("hashchange", function () {
-        window.scrollTo(window.scrollX, window.scrollY - 100);
-    });
-
-    $(document).ready(function () {
-        $('.js-datepicker').datepicker({
-            format: 'yyyy-mm-dd',
-            todayBtn: "linked",
-            language: "eu",
-            autoclose: true,
-            todayHighlight: true
-        });
-    });
-
-</script> */
